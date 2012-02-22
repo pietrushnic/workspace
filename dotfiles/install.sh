fa11a67cc865afd2ec7f1b8ddf20b1b1e5da6d60
pietrushnic <pietrushnic@gmail.com>
2012-02-22 17:37:02 +0100

    Dotfiles automated script update - screenrc added

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -3,8 +3,10 @@ mv $HOME/.vimrc $HOME/.vimrc.old
 mv $HOME/.gitconfig $HOME/.gitconfig.old
 rm $HOME/.vim.old
 mv $HOME/.vim $HOME/.vim.old
+mv $HOME/.screenrc $HOME/.screenrc.old
 
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/gitconfig $HOME/.gitconfig
 ln -s $PWD/vim $HOME/.vim
+ln -s $PWD/screenrc $HOME/.screenrc
 
