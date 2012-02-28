823fb29408fabb81f9a8c10d41f8085bf32cef01
pietrushnic <pietrushnic@gmail.com>
2012-02-28 19:46:58 +0100

    Dotfiles installation script update - bashrc added

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,9 +4,11 @@ mv $HOME/.gitconfig $HOME/.gitconfig.old
 rm $HOME/.vim.old
 mv $HOME/.vim $HOME/.vim.old
 mv $HOME/.screenrc $HOME/.screenrc.old
+mv $HOME/.bashrc $HOME/.bashrc.old
 
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/gitconfig $HOME/.gitconfig
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/screenrc $HOME/.screenrc
+ln -s $PWD/bashrc $HOME/.bashrc
 
